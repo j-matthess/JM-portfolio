e5066827f37b0c4ce10c15ebfcc4452c5e92de04
Jaxon Matthess <matthess.j06@gmail.com>
2025-03-03 00:48:56 -0600

    Week 7

--- Capstone-Project/js/scripts.js
+++ Capstone-Project/js/scripts.js
@@ -1,4 +1,10 @@
 $(document).ready(function(){
     // Verify JS and jQ loaded
     console.log("Ready!")
+
+    $(".titleText").fadeIn(400);
+    $(".navText").fadeIn(450);
+    $(".headText").fadeIn(500);
+    $(".bodyText").fadeIn(550);
+    $(".footerText").fadeIn(600);
 });
